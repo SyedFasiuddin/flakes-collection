8041caedcddbe672e3c698d883bb5a97056be897
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-05-28 13:53:22 +0530

    feat: boost c++

---
+++ boost-cpp.nix
@@ -0,0 +1,10 @@
+# Boost v1.81
+
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c2e511861a8f2f866a0a07149b7ec795d33f5591.tar.gz") {};
+in
+pkgs.mkShell {
+    buildInputs = with pkgs; [
+      boost
+    ];
+}
