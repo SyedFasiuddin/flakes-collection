e1ce45635cd21e8556a560e2220138c98e8c85df
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-06-12 19:58:11 +0530

    feat: simple template for new flakes

--- boost-cpp.nix
+++ boost-cpp.nix
@@ -4,7 +4,7 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/c2e511861a8f2f866a0a07149b7ec795d33f5591.tar.gz") {};
 in
 pkgs.mkShell {
-    buildInputs = with pkgs; [
-      boost
-    ];
+  buildInputs = with pkgs; [
+    boost
+  ];
 }
---
+++ template-flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A flake template";
+  input = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  output = {self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+          ];
+        };
+      }
+    );
+}
