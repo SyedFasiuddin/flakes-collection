b5054de6859e261ef9e8038e7e29f9d8e28296e2
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-06-26 18:18:27 +0530

    fix: wrong template flake fields

--- template-flake.nix
+++ template-flake.nix
@@ -1,10 +1,10 @@
 {
   description = "A flake template";
-  input = {
+  inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  output = {self, nixpkgs, flake-utils}:
+  outputs = {self, nixpkgs, flake-utils}:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
